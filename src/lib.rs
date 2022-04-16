124eedef917cacf8f12a66cd991dc31457da5c1f
Juliette Cordor <grassojames5@gmail.com>
2022-04-16 21:13:38 +1000

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,89 @@ fn get_bttv(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string(res))
 }
 
+struct EmoteData {
+    pub id: String,
+    pub code: String,
+    pub image_type: String,
+    pub user_id: String,
+}
+
+impl EmoteData {
+    fn from_object<'a, C>(cx: &mut C, obj: Handle<JsObject>) -> Self
+    where
+        C: Context<'a>,
+    {
+        let id = obj.get::<JsString, C, &str>(cx, "id").unwrap().value(cx);
+        let code = obj.get::<JsString, C, &str>(cx, "code").unwrap().value(cx);
+        let image_type = obj
+            .get::<JsString, C, &str>(cx, "imageType")
+            .unwrap()
+            .value(cx);
+        let user_id = obj
+            .get::<JsString, C, &str>(cx, "userId")
+            .unwrap()
+            .value(cx);
+
+        EmoteData {
+            id,
+            code,
+            image_type,
+            user_id,
+        }
+    }
+}
+
+struct ParsedResult {
+    pub emote: EmoteData,
+    pub index: i32,
+    pub urls: [String; 3],
+}
+
+fn vec_to_array<'a, C: Context<'a>>(vec: Vec<String>, cx: &mut C) -> JsResult<'a, JsArray> {
+    let a = JsArray::new(cx, vec.len() as u32);
+
+    for (i, s) in vec.iter().enumerate() {
+        let v = cx.string(s);
+        a.set(cx, i as u32, v)?;
+    }
+
+    Ok(a)
+}
+
+const SIZES: [u8; 3] = [1, 2, 3];
+
+fn parse_string(mut cx: FunctionContext) -> JsResult<JsArray> {
+    let string = cx.argument::<JsString>(0)?;
+    let emotes = cx.argument::<JsArray>(1)?;
+
+    let emotes_vec = emotes.to_vec(&mut cx)?;
+
+    let mut parsed: Vec<ParsedResult> = Vec::new();
+
+    for (index, emote) in emotes_vec.iter().enumerate() {
+        let emote_obj = emote
+            .downcast::<JsObject, FunctionContext>(&mut cx)
+            .unwrap();
+
+        let casted_emote = EmoteData::from_object(&mut cx, emote_obj);
+
+        let urls = SIZES.clone().map(|size| {
+            format!(
+                "https://cdn.betterttv.net/emote/{}/{}x",
+                casted_emote.id, size
+            )
+        });
+
+        let result = ParsedResult {
+            emote: casted_emote,
+            index: index as i32,
+            urls,
+        };
+    }
+
+    vec_to_array(parsed, &mut cx)
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("getBTTV", get_bttv)?;
