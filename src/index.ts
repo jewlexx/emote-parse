ccfa2189d7a12d02f2d80718b9a14ca232a77dbf
Juliette Cordor <grassojames5@gmail.com>
2022-04-16 20:40:45 +1000

    fixes

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ function parseJson(callback: () => string) {
   return () => JSON.parse(callback());
 }
 
+type EmoteUrl = `https://cdn.betterttv.net/emote/${string}/${1 | 2 | 3}x`;
+
 export type ParseReturn = {
   id: string;
   code: string;
   index: number;
   userId: string;
+  urls: EmoteUrl[];
   imageType: 'png' | 'jpg';
 }[];
 
@@ -16,12 +19,17 @@ export function parseString(str: string, emotes: any[]): ParseReturn {
   const numbers: ParseReturn = [];
 
   emotes.forEach((emote) => {
+    const urls = [1, 2, 3].map((v) => {
+      return `https://cdn.betterttv.net/emote/${emote.id}/${v}x`;
+    });
+
     function getIndex(startIndex?: number) {
       const index = str.indexOf(emote.code, startIndex);
 
       if (index != -1) {
         numbers.push({
           ...emote,
+          urls,
           index,
         });
         getIndex(index + 1);
